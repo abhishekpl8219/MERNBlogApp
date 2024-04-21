ba25f2607b9eaf1c19a1f52b42da4a3582ef4d00
abhishek <abhishekpl8219@gmail.com>
2024-04-21 07:18:28 +0530

    complete the post page UI

---
+++ client/src/pages/CreatePost.jsx
@@ -0,0 +1,39 @@
+import { Button, FileInput, Select, TextInput } from 'flowbite-react'
+import React from 'react';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
+
+export default function CreatePost() {
+  return (
+    <div className='p-3 max-w-3xl mx-auto min-h-screen'>
+      <h1 className='text-center text-3xl my-7 font-semibold'> Create a Post</h1>
+    <form className='flex flex-col gap-4 '>
+        <div className='flex flex-col gap-4 sm:flex-row justify-between'>
+            <TextInput type='text ' placeholder='Title' id='title' required className='flex-1' ></TextInput>
+            <Select>
+                <option value = "uncategorized">
+                    select a category
+                </option>
+                <option value = "javascript">
+                    JavaScript
+                </option>
+                <option value = "reactjs">
+                    React.js
+                </option>
+                <option value = "nextjs">
+                    Next.js
+                </option>
+            </Select>
+
+        </div>
+        <div className='flex gap-4 items-center justify-between border-4 border-teal-500 p-3 border-dotted'>
+         <FileInput type='file' accept='image/*' />
+         <Button type='button ' gradientDuoTone='purpleToBlue' outline>Upload image</Button>
+     
+        </div>
+        <ReactQuill theme="snow" placeholder='Write something....' className='h-72 mb-12' required></ReactQuill>
+         <Button type="submit "gradientDuoTone='purpleToBlue' outline>Publish</Button>
+    </form>
+    </div>
+  )
+}
